7f0a3fcdc94687ea43c0fe97cb1a13c30041433d
unicsage <teddyprince22@gmail.com>
2024-02-13 17:25:46 +0000

    module fields

---
+++ public/backend/router/POST_handlers.js
@@ -0,0 +1,3 @@
+const { RegistrationModel } = require('../../database/schematics');
+
+
--- public/database/schematics.js
+++ public/database/schematics.js
@@ -4,8 +4,14 @@ const mongoose = require("mongoose");
 // DEFINING SCHEMATICS
 const registration_schema = new mongoose.Schema({
     email: {type: String, required: true, unique: true},
-
-
+    businessName: { type: String, required: true },
+    natureOfBusiness: {type: String, required: this},
+    businessType: { type: String, required: true },
+    location: { type: String, required: true },
+    address: { type: String, required: true },
+    contact: { type: String, required: true },
+    businessLogo: { type: String }, 
+    nationality: { type: String, required: true },
 
 });
 
