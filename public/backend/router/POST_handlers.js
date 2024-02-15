6fd1169d7571b0cfe413d87d139f9c83c2b8597d
unicsage <teddyprince22@gmail.com>
2024-02-15 18:21:57 +0000

    logic of bio user

--- public/backend/router/POST_handlers.js
+++ public/backend/router/POST_handlers.js
@@ -1,3 +1,26 @@
 const { RegistrationModel } = require('../../database/schematics');
 
+const registration_handler = async (req, res, next) => {
 
+    var email = req.body.email;
+    const  businessName = req.body.businessName;
+    const  natureOfBusiness = req.body.natureOfBusiness;
+    const  businessType = req.body.businessType;
+    const  location = req.body.location;
+    let address = req.body.address;
+    let contact = req.body.contact;
+    let businessLogo = req.body.businessLogo
+    let nationality = req.body.nationality;
+    // console.log(email, businessName, natureOfBusiness)
+
+    try { 
+        var email = await RegistrationModel.find({ "email": email });
+        console.log(email)
+
+    } catch (error) {
+        
+    }
+
+}
+
+module.exports = { registration_handler}
